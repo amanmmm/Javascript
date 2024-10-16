4805c8db35f6278e1d217a563574b87123bc354e
amanmmm <2023021008@mmmut.ac.in>
2024-10-16 23:23:42 +0530

    Friends

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
-import React, { useState } from "react";
-import Card from "./Components/card";
-import Music from "./Components/Music";
+import React, { useState } from 'react';
+import Card1 from './Components/Card1';
+
 function App() {
-  const [val, b] = useState({
-    name: "Music",
-    isBann: false,
-  });
+  const data = [
+    {
+      name: "John",
+      profession: "Painter",
+      image: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=600",
+      friends: false
+    },
+    {
+      name: "Jane",
+      profession: "Writer",
+      image: "https://images.pexels.com/photos/1300402/pexels-photo-1300402.jpeg?auto=compress&cs=tinysrgb&w=600",
+      friends: false
+    },
+    {
+      name: "Mike",
+      profession: "Engineer",
+      image: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=600",
+      friends: false
+    }
+  ];
+
+  const [realdata, setdata] = useState(data);
+
+  const toggleFriendship = (index) => {
+    setdata((prev) => {
+      return prev.map((item, idx) => {
+        if (idx === index) {
+         
+          return {...item, friends: !item.friends};
+        }
+        return item;
+      });
+    });
+  };
+   
+
   return (
-    <div>
-      <h1>Value : {val.isBann.toString()}</h1>
-      <button
-        onClick={() => b({...val, isBann: !val.isBann })}
-        className={`${
-          val.isBann ? "bg-blue-500" : "bg-red-500"
-        } rounded-full px-3 py-2 text-black text-xs`}
-      >
-        change
-      </button>
+    <div className='w-full h-screen bg-zinc-300 flex gap-4 items-center justify-center'>
+      {realdata.map((item, index) => (
+        <Card1 values={item} key={index} handle={() => toggleFriendship(index)} />
+      ))}
     </div>
   );
 }
